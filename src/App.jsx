b1c7048caf8feb7e53b84a367121c75c89ed5a1d
Gustavo Fonte <gusfonte99@gmail.com>
2023-11-16 16:30:36 +0000

    create pages/navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import './App.css'
+import Navbar from "./components/Navbar";
 
 function App() {
 
   return (
+    <>
+    <Navbar />
+
     <Routes>
       <Route path="/" element={<h1>HELLO</h1>}/>
       <Route path="/dates" element={<h1>Dates List page</h1>}/>
     </Routes>
+    </>
   )
 }
 
