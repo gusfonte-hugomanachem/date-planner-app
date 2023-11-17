e0d2ee40f1a9fa1cf69a22ea0516a679798b28bd
Hugo Manachem <hugomanachem.pro@gmail.com>
2023-11-17 10:56:51 +0100

    add Date/see details buttons

--- src/pages/DateList.jsx
+++ src/pages/DateList.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 import "../App.css";
@@ -22,6 +23,10 @@ function DateList() {
     <div id="date-list-page">
       <h1>Date List</h1>
 
+      <Link to={"/dates/create"}>
+        <button>Add a Date</button>
+      </Link>
+
       <div className="dates-container">
         {dateList.map((date) => {
           return (
@@ -30,6 +35,9 @@ function DateList() {
               <p>Time: {date.time}</p>
               <p>Place: {date.place}</p>
               <p>Description: {date.description}</p>
+              <Link to={"/"}>
+                <button>See details</button>
+              </Link>
             </div>
           );
         })}
