e4ca09d9f9b1178e771e8807c46ad4ddf2ef67e7
Hugo Manachem <hugomanachem.pro@gmail.com>
2023-11-23 14:56:22 +0100

    Test to deploy Netlify function

--- src/components/GoogleMapsScriptLoader.jsx
+++ src/components/GoogleMapsScriptLoader.jsx
@@ -1,19 +1,24 @@
 import { useEffect, useState } from "react";
-function GoogleMapsScriptLoader ()  {
-  const googleMapsScriptSrc = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places&callback=Function.prototype`
-  let [state, setState] = useState(googleMapsScriptSrc ? "loading" : "idle");
-  
+function GoogleMapsScriptLoader() {
+ // let [state, setState] = useState(googleMapsScriptSrc ? "loading" : "idle");
+
   useEffect(() => {
-    if (!googleMapsScriptSrc) {
-      setState("idle");
-      return;
-     }
+    axios
+      .get(`${import.meta.env.VITE_NETLIFY_URL}`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => console.log(err));
+    // if (!googleMapsScriptSrc) {
+    //   setState("idle");
+    //   return;
+    // }
     let script = document.querySelector(`script[src="${googleMapsScriptSrc}"]`);
-    
+
     const handleScript = (e) => {
       setState(e.type === "load" ? "ready" : "error");
     };
-    
+
     if (!script) {
       script = document.createElement("script");
       script.type = "application/javascript";
@@ -23,17 +28,17 @@ function GoogleMapsScriptLoader ()  {
       script.addEventListener("load", handleScript);
       script.addEventListener("error", handleScript);
     }
-   
-   script.addEventListener("load", handleScript);
-   script.addEventListener("error", handleScript);
-   
-   return () => {
-     script.removeEventListener("load", handleScript);
-     script.removeEventListener("error", handleScript);
-   };
+
+    script.addEventListener("load", handleScript);
+    script.addEventListener("error", handleScript);
+
+    return () => {
+      script.removeEventListener("load", handleScript);
+      script.removeEventListener("error", handleScript);
+    };
   }, [googleMapsScriptSrc]);
-  
+
   return state;
-};
+}
 
 export default GoogleMapsScriptLoader;
