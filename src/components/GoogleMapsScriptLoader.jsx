9022210d5119eb4804f01af746a71882def2eb53
Hugo Manachem <hugomanachem.pro@gmail.com>
2023-11-23 16:23:31 +0100

    Use API key on Netlify + delete Netlify function

--- src/components/GoogleMapsScriptLoader.jsx
+++ src/components/GoogleMapsScriptLoader.jsx
@@ -1,47 +1,39 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
-
-function GoogleMapsScriptLoader() {
-
- // let [state, setState] = useState(googleMapsScriptSrc ? "loading" : "idle");
-
+function GoogleMapsScriptLoader ()  {
+  const url = `https://maps.googleapis.com/maps/api/js?key=${process.env.API_KEY}&libraries=places&callback=Function.prototype`
+  let [state, setState] = useState(url ? "loading" : "idle");
+  
   useEffect(() => {
-    axios
-      .get(`${import.meta.env.VITE_NETLIFY_URL}/api`)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((err) => console.log(err));
-    // if (!googleMapsScriptSrc) {
-    //   setState("idle");
-    //   return;
-    // }
-    //let script = document.querySelector(`script[src="${googleMapsScriptSrc}"]`);
-
-    // const handleScript = (e) => {
-    //   setState(e.type === "load" ? "ready" : "error");
-    // };
-
-    // if (!script) {
-    //   script = document.createElement("script");
-    //   script.type = "application/javascript";
-    //   script.src = googleMapsScriptSrc;
-    //   script.async = true;
-    //   document.body.appendChild(script);
-    //   script.addEventListener("load", handleScript);
-    //   script.addEventListener("error", handleScript);
-    // }
-
-    // script.addEventListener("load", handleScript);
-    // script.addEventListener("error", handleScript);
-
-    // return () => {
-    //   script.removeEventListener("load", handleScript);
-    //   script.removeEventListener("error", handleScript);
-    // };
-  }, []);
-// }, [googleMapsScriptSrc]);
-  //return state;
-}
+    if (!url) {
+      setState("idle");
+      return;
+     }
+    let script = document.querySelector(`script[src="${url}"]`);
+    
+    const handleScript = (e) => {
+      setState(e.type === "load" ? "ready" : "error");
+    };
+    
+    if (!script) {
+      script = document.createElement("script");
+      script.type = "application/javascript";
+      script.src = url;
+      script.async = true;
+      document.body.appendChild(script);
+      script.addEventListener("load", handleScript);
+      script.addEventListener("error", handleScript);
+    }
+   
+   script.addEventListener("load", handleScript);
+   script.addEventListener("error", handleScript);
+   
+   return () => {
+     script.removeEventListener("load", handleScript);
+     script.removeEventListener("error", handleScript);
+   };
+  }, [url]);
+  
+  return state;
+};
 
 export default GoogleMapsScriptLoader;
